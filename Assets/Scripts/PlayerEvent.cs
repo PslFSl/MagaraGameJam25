e65352788373ed5230bf19c6f60dead32756ab2f
Fatih Özer <105015070+PslFSl@users.noreply.github.com>
2025-09-18 23:45:57 +0300

    Turn code

--- Assets/Scripts/PlayerEvent.cs
+++ Assets/Scripts/PlayerEvent.cs
@@ -13,11 +13,17 @@ public class PlayerEvent : MonoBehaviour
     [Header("Ground Check")]
     public LayerMask groundLayer;
     public Transform feetPos;
-    float checkRad=0.1f;
+    float checkRad=1f;
     public bool isGrounded;
+    [Header("Rotate")]
+    public float firstRot;
+    public float lastRot;
+    bool turnRight;
+    bool turnLeft;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        lastRot = transform.rotation.z;
     }
     void Update()
     {
@@ -35,7 +41,41 @@ public class PlayerEvent : MonoBehaviour
             rb.gravityScale += 1 * Time.deltaTime*fallSpeed;
         }
         if (isGrounded) { rb.gravityScale = 1; }
-        
+
+        transform.rotation = Quaternion.Euler(0, 0, firstRot);
+
+
+        if (Input.GetKeyDown(KeyCode.E)&&rb.linearVelocity.y>0)
+        {
+             lastRot -= 90;
+            turnRight = true;
+            
+        }
+        if (Input.GetKeyDown(KeyCode.Q) && rb.linearVelocity.y > 0)
+        {
+            lastRot += 90;
+            turnLeft = true;
+
+        }
+
+
+        if (firstRot > lastRot&&turnRight==true)
+        {
+            firstRot -= Time.deltaTime * 150f;
+        }
+        else
+        {
+            turnRight = false;
+        }
+        if (firstRot < lastRot && turnLeft == true)
+        {
+            firstRot += Time.deltaTime * 150f;
+        }
+        else
+        {
+            turnLeft = false;
+        }
+
     }
     
     
