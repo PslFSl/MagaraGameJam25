e966e91d26bcaed9eb7f154fc92cc60f27a270b4
Fatih Özer <105015070+PslFSl@users.noreply.github.com>
2025-09-19 18:20:33 +0300

    3

--- Assets/Scripts/ElectroTrap.cs
+++ Assets/Scripts/ElectroTrap.cs
@@ -2,7 +2,15 @@ using UnityEngine;
 
 public class ElectroTrap : MonoBehaviour
 {
-    
+    public float speed;
+    public float distance;
+
+
+    private void Update()
+    {
+        float x = Mathf.PingPong(Time.deltaTime*speed,distance*2)-distance;
+        transform.position= new Vector3(transform.position.x + x ,transform.position.y,transform.position.z);
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
